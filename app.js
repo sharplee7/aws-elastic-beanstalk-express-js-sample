a8ef5034377fa1a9c3768b4f6016de1af78e67cf
Lee <euideok@3c06303905c0.ant.amazon.com>
2022-03-15 16:02:33 +0900

    second update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! and this applicaiton was changed.'));
+app.get('/', (req, res) => res.send('Hello World! and this applicaiton was changed.<br>'));
+app.get('/', (req, res) => res.send('added new line.<br>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
