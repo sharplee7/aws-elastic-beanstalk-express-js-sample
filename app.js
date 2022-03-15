9600d13eb15bd1d5f717a6abe6591449bde92ac5
Lee <euideok@3c06303905c0.ant.amazon.com>
2022-03-15 16:37:08 +0900

    modified new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! and this applicaiton was changed. and modified again<br>'));
+app.get('/', (req, res) => res.send('Hello World! and this applicaiton was changed. and modified again and delete~~~~'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
